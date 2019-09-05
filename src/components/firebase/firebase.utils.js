60fa4521477e2a0bcf2f257b65342a46af9cc2a2
DeanDK <deanbozic12@gmail.com>
2019-09-05 17:47:07 +0200

    created firebase method for adding new users to firestore

--- src/components/firebase/firebase.utils.js
+++ src/components/firebase/firebase.utils.js
@@ -12,6 +12,33 @@ const config = {
   appId: "1:77183792125:web:d98c5cf85376cfaecfdf94"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) {
+        return;
+    }
+
+    const userRef = firestore.doc(`users/${userAuth.id}`);
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
